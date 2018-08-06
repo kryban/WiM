cb39f1a5ffcf3f12fcafa6c605c7054378a7d67e
skippr81 <eschip81@gmail.com>
2018-08-06 14:22:56 +0200

    Vette Xtreme switch toegevoegd

---
+++ WiM/WiMcore/Config/TaskElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiMcore.Config
+{
+    public class TaskElement : ConfigurationElement
+    {
+        [ConfigurationProperty("title", IsRequired = true, IsKey = true)]
+        public string Title => (string) base["title"];
+
+        [ConfigurationProperty("activityType", IsRequired = false)]
+        public string ActivityType => (string) base["activityType"];
+    }
+}
---
+++ WiM/WiMcore/Config/TaskTemplateElementCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiMcore.Config
+{
+    public class TaskTemplateElementCollection : ConfigurationElementCollection, IEnumerable<TaskElement>
+    {
+        private const string PropertyName = "task";
+
+        public override ConfigurationElementCollectionType CollectionType =>
+            ConfigurationElementCollectionType.BasicMapAlternate;
+
+        protected override string ElementName => PropertyName;
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new TaskElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((TaskElement)element).Title;
+        }
+
+        public TaskElement this[int index] => (TaskElement)BaseGet(index);
+
+        public new IEnumerator<TaskElement> GetEnumerator()
+        {
+            return Enumerable.Range(0, Count).Select(i => this[i]).GetEnumerator();
+        }
+    }
+}
