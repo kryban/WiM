23b07e23099fb2eca14d3d227cdf730c361d38af
kry <kry.ban@gmail.com>
2019-06-13 22:33:27 +0200

    additional steps towards specflow step implementations

---
+++ WiM/WiM.Specs/Task.feature
@@ -0,0 +1,24 @@
+﻿#Feature: Taken
+#	In order to avoid silly mistakes
+#	As a math idiot
+#	I want to be told the sum of two numbers
+
+#@mytag
+#Scenario: Add two numbers
+#	Given I have entered 50 into the calculator
+#	And I have entered 70 into the calculator
+#	When I press add
+#	Then the result should be 120 on the screen
+
+Feature: Task
+In order to link taks to the correct PBI
+As a Azure DevOps user
+I want to be able to search for a specific PBI
+
+@mytag
+Scenario: Find PBI with number
+	Given i have entered a valid number as searchcriteria
+	And that number equals with an id of a PBI
+	When i press search
+	Then the PBI is selected as the current PBI
+
---
+++ WiM/WiM.Specs/TaskSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using WiM.Core;
+using mock
+
+namespace WiM.Specs
+{
+    [Binding]
+    public class TaskSteps
+    {
+        private TfsController sut = new TfsController();
+        private int workitemId = 0;
+
+        [Given(@"i have entered a valid number as searchcriteria")]
+        public void GivenIHaveEnteredAValidNumberAsSearchcriteria()
+        {
+            workitemId = 1;
+        }
+
+        [Given(@"that number equals with an id of a PBI")]
+        public void GivenThatNumberEqualsWithAnIdOfAPBI()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"i press search")]
+        public void WhenIPressSearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the PBI is selected as the current PBI")]
+        public void ThenThePBIIsSelectedAsTheCurrentPBI()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
