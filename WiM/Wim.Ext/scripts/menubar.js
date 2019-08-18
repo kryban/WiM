38d6761fa2e6db633c6528d92c7e93ad9c531f42
kry <kry.ban@gmail.com>
2019-08-18 23:01:27 +0200

    First steps  for the settings form, embedded in a dialog

--- WiM/Wim.Ext/scripts/menubar.js
+++ WiM/Wim.Ext/scripts/menubar.js
@@ -5,37 +5,36 @@ VSS.require(["VSS/Controls", "VSS/Controls/Menus"], function (Controls, Menus) {
     // todo: menuitems moeten op basis van configuratie bij elkaar gegenereerd worden. 
     // dus eerst configs ophalen (https://docs.microsoft.com/en-us/azure/devops/extend/develop/data-storage?view=azure-devops&viewFallbackFrom=vsts)
     // en vervolgens op basis van de opgehaalde data onderstaande string gegenereren.
-    var menuItems = [
-        {
-            id: "menu-setting", text: "Settings", icon: "icon-settings", childItems:
-                [{
-                    id: "switch", text: "Switch", childItems:
-                        [
-                            { id: "xtreme", text: "Xtreme" },
-                            { id: "committers", text: "Committersss" },
-                            { id: "test", text: "Test" }
-                        ]
-                },
-                {
-                    id: "manage-teams", text: "Manage teams", childItems:
-                        [   { id: "add-new-team", text: "Add new team" },
-                            { id: "delete-team", text: "Delete team" }
-                        ]
-                },
-                {
-                    id: "configure-team-tasks", text: "Configure team tasks", childItems:
-                        [
-                            { id: "team-xtreme", text: "Team Xtreme" },
-                            { id: "team-committers", text: "Team Committers" }
-                        ]
-                }
-                ]
-        },
-        { separator: true }, // Default separator
-        { id: "menu-help", text: "Help", icon: "icon-help", tag: "test" }
+    var menuItems =
+        [
+            {id: "menu-setting", text: "Settings", icon: "icon-settings", childItems:
+                    [
+                        {id: "switch", text: "Switch", childItems:
+                            [
+                                { id: "xtreme", text: "Xtreme" },
+                                { id: "committers", text: "Committersss" },
+                                { id: "test", text: "Test" }
+                                ]
+                        },
+                        {id: "manage-teams", text: "Manage teams", childItems:
+                            [   { id: "add-new-team", text: "Add new team" },
+                                { id: "delete-team", text: "Delete team" }
+                            ]
+                        },
+                        {id: "configure-team-tasks", text: "Configure team tasks", childItems:
+                            [
+                                { id: "team-xtreme", text: "Team Xtreme" },
+                                { id: "team-committers", text: "Team Committers" }
+                            ]
+                        }
+                    ]
+            },
+            { separator: true }, // Default separator
+            { id: "menu-help", text: "Help", icon: "icon-help", tag: "test" }
 
     ];
 
+    // stukje abrakadabra
     var menubarOptions = {
         items: menuItems,
         executeAction: function (args) {
--- WiM/Wim.Ext/scripts/settings.js
+++ WiM/Wim.Ext/scripts/settings.js
@@ -8,6 +8,86 @@ function ConfigureTeams(command) {
     OpenConfiguratieDialoog(command);
 }
 
+function OpenConfiguratieDialoog(title) {
+    // // customg try out
+    //VSS.getService(VSS.ServiceIds.Dialog).then(function (dialogService) {
+    //    var extensionCtx = VSS.getExtensionContext();
+    //    // Build absolute contribution ID for dialogContent
+    //    var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".dialogContent";
+
+    //    // Show dialog
+    //    var dialogOptions = {
+    //        title: title
+    //        , width: 500
+    //        //,height: 75
+    //    };
+
+    //    dialogService.openDialog(contributionId, dialogOptions);
+    //});
+
+    // // settings
+    VSS.getService(VSS.ServiceIds.Dialog).then(function (dialogService) {
+        var extensionCtx = VSS.getExtensionContext();
+        // Build absolute contribution ID for dialogContent
+        //var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".teamsettings-form";
+        //var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".beta";
+        //var contributionId = "Bandik.WimDevOpExtension.beta";
+          var contributionId = "Bandik.WimDevOpExtension.teamsettings-form";
+
+        // Show dialog
+        var dialogOptions = {
+            title: "My Dialog: " + contributionId + "||" + title,
+            width: 800,
+            height: 600
+        };
+
+        dialogService.openDialog(contributionId, dialogOptions);
+    });
+
+    // advanced
+    //VSS.getService(VSS.ServiceIds.Dialog).then(
+    //    function (dialogService) {
+    //        var registrationForm;
+    //        var extensionCtx = VSS.getExtensionContext();
+    //        var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".teamsettings-form";
+
+    //        var dialogOptions = {
+    //            title: "Teamsettings Form: " + title +" --" + contributionId,
+    //            width: 800,
+    //            height: 600,
+    //            getDialogResult: function () {
+    //                // Get the result from registrationForm object
+    //                return registrationForm ? registrationForm.getFormData() : null;
+    //            },
+    //            okCallback: function (result) {
+    //                // Log the result to the console
+    //                console.log(JSON.stringify(result));
+    //            }
+    //        };
+
+    //        dialogService.openDialog(contributionId, dialogOptions).then(
+    //            function (dialog) {
+    //                // Get registrationForm instance which is registered in registrationFormContent.html
+    //                dialog.getContributionInstance("teamsettings-form").then(
+    //                    function (teamSettingsFormInstance) {
+
+    //                        // Keep a reference of registration form instance (to be used above in dialog options)
+    //                        registrationForm = teamSettingsFormInstance;
+
+    //                        // Subscribe to form input changes and update the Ok enabled state
+    //                        registrationForm.attachFormChanged(function (isValid) {
+    //                            dialog.updateOkButton(isValid);
+    //                        });
+
+    //                        // Set the initial ok enabled state
+    //                        registrationForm.isFormValid().then(function (isValid) {
+    //                            dialog.updateOkButton(isValid);
+    //                        });
+    //                    });
+    //            });
+    //});
+}
+
 function GetTeamSettings() {
     // Get data service
     VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
