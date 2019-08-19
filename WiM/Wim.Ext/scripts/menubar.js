24deb799a222e1273083d5c6de2b913586a4cf0c
kry <kry.ban@gmail.com>
2019-08-19 22:21:52 +0200

    Een grote ellende met de execution order in javascript. Alvast wat gecommit, maar werkt nog voor geen meter. Kutjavascript.

--- WiM/Wim.Ext/scripts/menubar.js
+++ WiM/Wim.Ext/scripts/menubar.js
@@ -1,42 +1,102 @@
 ﻿
-VSS.require(["VSS/Controls", "VSS/Controls/Menus"], function (Controls, Menus) {
+
+
+
+VSS.require(["VSS/Controls", "VSS/Controls/Menus"], function (Controls, Menus){
     var container = $(".menu-bar");
 
-    // todo: menuitems moeten op basis van configuratie bij elkaar gegenereerd worden. 
+    var team1 = { text: "Xtreme" };
+    var team2 = { text: "Committers" };
+    var team3 = { text: "Test" };
+
+    //DeleteTeams();
+    CreateTeams();
+    //GetTeams();
+
+    var teams = [team1, team2, team3];
+    //var teams = configuredTeams;
+
+    var teamMenuItems = [];
+
+    teams.forEach(
+        function (element) {
+            teamMenuItems.push(
+                { id: element.text.toLowerCase(), text: element.text }
+            );
+        }
+    );
+
+    var teamItemsStringified = JSON.stringify(teamMenuItems);
+
+
+    // docs worden gegroepeerd op basis van Collections
+    // teams behoren tot de collectio Wim
+    // taken moeten behoren tot collections van teams, zo kunnen ze per team herkend worden
+    // bij het getten kunnen ze dan de teamnaam als argument mee krijgen, waarna de tasks geretourneerd worden.
+    var teamTaskItems = '[{"id": "team-xtreme", "text": "Team XtremeNeww" },{ "id": "team-committers", "text": "Team CommittersNeww" }]';
+
+    var menuItems =
+        '[' +
+            '{'+
+                '"id":"menu-setting", "text":"Settings", "icon":"icon-settings", "childItems":'+
+                    '['+
+                        '{'+
+                            '"id": "switch", "text": "Switch", "childItems":'+
+                                teamItemsStringified+
+                        '},'+
+                        '{'+
+                            '"id": "manage-teams", "text": "Manage teams", "childItems":'+
+                                '[{ "id": "add-new-team", "text": "Add new team" },'+
+                                 '{ "id": "delete-team", "text": "Delete team" }'+
+                                ']'+
+                        '},'+
+                        '{'+
+                            '"id": "configure-team-tasks", "text": "Configure team tasks", "childItems":'+
+                                teamTaskItems +
+                        '}'+
+                    ']'+
+            '},'+
+            '{ "separator": "true" },'+
+            '{ "id": "menu-help", "text": "Help", "icon": "icon-help", "tag": "test" }'+
+        ']';
+
+    var menuItemsParsed = JSON.parse(menuItems);
+
+    // todo: menuitems moeten op basis van configuratie bij elkaar gegenereerd worden.
     // dus eerst configs ophalen (https://docs.microsoft.com/en-us/azure/devops/extend/develop/data-storage?view=azure-devops&viewFallbackFrom=vsts)
     // en vervolgens op basis van de opgehaalde data onderstaande string gegenereren.
-    var menuItems =
-        [
-            {id: "menu-setting", text: "Settings", icon: "icon-settings", childItems:
-                    [
-                        {id: "switch", text: "Switch", childItems:
-                            [
-                                { id: "xtreme", text: "Xtreme" },
-                                { id: "committers", text: "Committersss" },
-                                { id: "test", text: "Test" }
-                                ]
-                        },
-                        {id: "manage-teams", text: "Manage teams", childItems:
-                            [   { id: "add-new-team", text: "Add new team" },
-                                { id: "delete-team", text: "Delete team" }
-                            ]
-                        },
-                        {id: "configure-team-tasks", text: "Configure team tasks", childItems:
-                            [
-                                { id: "team-xtreme", text: "Team Xtreme" },
-                                { id: "team-committers", text: "Team Committers" }
-                            ]
-                        }
-                    ]
-            },
-            { separator: true }, // Default separator
-            { id: "menu-help", text: "Help", icon: "icon-help", tag: "test" }
+    //var menuItems =
+    //    [
+    //        {id: "menu-setting", text: "Settings", icon: "icon-settings", childItems:
+    //                [
+    //                    {id: "switch", text: "Switch", childItems:
+    //                        [
+    //                            { id: "xtreme", text: "Xtreme" },
+    //                            { id: "committers", text: "Committers" },
+    //                            { id: "test", text: "Test" }
+    //                            ]
+    //                    },
+    //                    {id: "manage-teams", text: "Manage teams", childItems:
+    //                        [   { id: "add-new-team", text: "Add new team" },
+    //                            { id: "delete-team", text: "Delete team" }
+    //                        ]
+    //                    },
+    //                    {id: "configure-team-tasks", text: "Configure team tasks", childItems:
+    //                        [
+    //                            { id: "team-xtreme", text: "Team Xtreme" },
+    //                            { id: "team-committers", text: "Team Committers" }
+    //                        ]
+    //                    }
+    //                ]
+    //        },
+    //        { separator: true }, // Default separator
+    //        { id: "menu-help", text: "Help", icon: "icon-help", tag: "test" }
 
-    ];
+    //];
 
     // stukje abrakadabra
     var menubarOptions = {
-        items: menuItems,
+        items: menuItemsParsed,
         executeAction: function (args) {
             var command = args.get_commandName();
             menuBarAction(command);
--- WiM/Wim.Ext/scripts/settings.js
+++ WiM/Wim.Ext/scripts/settings.js
@@ -1,6 +1,11 @@
 ﻿
 //https://docs.microsoft.com/en-us/azure/devops/extend/develop/data-storage?view=azure-devops&viewFallbackFrom=vsts
 
+//get settings
+//http://krylp:8080/tfs/DefaultCollection/_apis/ExtensionManagement/InstalledExtensions/bandik/WimDevOpExtension/Data/Scopes/Default/Current/Collections/WimCollection/Documents
+
+var TeamSettingsCollectionName = "WimCollection";
+
 function ConfigureTeams(command) {
 
     //OpenTeamSettingsDialogAdvanced(command);
@@ -9,36 +14,19 @@ function ConfigureTeams(command) {
 }
 
 function OpenConfiguratieDialoog(title) {
-    // // customg try out
-    //VSS.getService(VSS.ServiceIds.Dialog).then(function (dialogService) {
-    //    var extensionCtx = VSS.getExtensionContext();
-    //    // Build absolute contribution ID for dialogContent
-    //    var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".dialogContent";
 
-    //    // Show dialog
-    //    var dialogOptions = {
-    //        title: title
-    //        , width: 500
-    //        //,height: 75
-    //    };
-
-    //    dialogService.openDialog(contributionId, dialogOptions);
-    //});
-
-    // // settings
     VSS.getService(VSS.ServiceIds.Dialog).then(function (dialogService) {
         var extensionCtx = VSS.getExtensionContext();
+
         // Build absolute contribution ID for dialogContent
         //var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".teamsettings-form";
-        //var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".beta";
-        //var contributionId = "Bandik.WimDevOpExtension.beta";
-          var contributionId = "Bandik.WimDevOpExtension.teamsettings-form";
+        var contributionId = extensionCtx.publisherId + "." + extensionCtx.extensionId + ".manage-teams";
 
         // Show dialog
         var dialogOptions = {
-            title: "My Dialog: " + contributionId + "||" + title,
-            width: 800,
-            height: 600
+            title: "Add new team: "
+            //width: 300,
+            //height: 200
         };
 
         dialogService.openDialog(contributionId, dialogOptions);
@@ -88,41 +76,187 @@ function OpenConfiguratieDialoog(title) {
     //});
 }
 
+function DeleteTeams() {
+    console.log("DeleteTeams() executed.");
+    DeleteAllTeamSettings();
+}
+
+
+function CreateTeams() {
+    console.log("CreateTeams() executing");
+    SetTeamSettingsNew("Xtreme");
+    console.log("CreateTeamsNew() executed.")
+    //SetTeamSettings("Committers");
+    //SetTeamSettings("Test");
+    //SetTeamSettings("NieuweTest");
+    //console.log("CreateTeams() executing");
+
+    //promises = [];
+    //promises.push(SetTeamSettingsNew("Xtreme"));
+    //promises.push(SetTeamSettingsNew("Committers"));
+    //promises.push(SetTeamSettingsNew("Test"));
+    //promises.push(SetTeamSettingsNew("NieuweTest"));
+
+    //Promise.all(promises).then(console.log("CreateTeamsNew() executed."));
+}
+
+
+function GetTeams() {
+    console.log("GetTeams() executed");
+    GetAllTeamSettings();
+}
+
+function DeleteAllTeamSettings() {
+
+    VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
+        // Get all document under the collection
+        dataService.getDocuments(TeamSettingsCollectionName).then(function (docs) {
+            //console.log("There are " + docs.length + " in the collection.");
+            docs.forEach(
+                function (element) {
+                    DeleteTeamSettings(dataService, element.id);
+                }
+            );
+        });
+    });
+}
+
+function SetTeamSettingsNew(teamName) {
+
+    VSS.getService(VSS.ServiceIds.ExtensionData).then(
+        function (dataservice) {
+            getExistingSettings(dataservice, function () {
+                checkIfExistBeforeAdding(docsNew, teamName, function () {
+                    if (result !== undefined) {
+                        addSetting(dataservice, teamName);
+                    }
+                });
+            });
+
+        });
+}
+
+var docsNew = [];
+
+function getExistingSettings(dataservice, callback) {
+
+    dataservice.getDocuments(TeamSettingsCollectionName).then(
+        function (docs) {
+            console.log("GetAllTeamSettingsNew :" + docs.length);
+
+            docs.forEach(
+                function (element) {
+                    docsNew.push(element);
+                }
+            );
+        }
+    );
+
+    callback();
+}
+
+var result;
+
+function checkIfExistBeforeAdding(docs, teamName, callback) {
+    result = docs.find(obj => { return obj.text === teamName; });
+    console.log("checkNew: " + result);
+    callback();
+}
+
+function addSetting(dataService, teamName) {
+    var newDoc = {
+        text: teamName
+    };
+
+    dataService.createDocument(TeamSettingsCollectionName, newDoc).then(function (doc) {
+        // Even if no ID was passed to createDocument, one will be generated
+        console.log("SetTeamSetting (CreateTeamsNew) : " + doc.text);
+    });
+
+    console.log("SettingNEw NOT exists.");
+}
+
+some_3secs_function(some_value, function () {
+        some_5secs_function(other_value, function () {
+                some_8secs_function(third_value, function () {
+                        //All three functions have completed, in order.
+        });
+    });
+});
+
+
+function SetTeamSettings(teamName) {
+    // Get data service
+    VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
+
+        var temp = [];
+        var result;
+
+        // Get all document under the collection
+        dataService.getDocuments(TeamSettingsCollectionName).then(
+            function (docs) {
+            //console.log("There areee " + docs.length + " in the collection in GetAllTeamSettings function.");
+            console.log("GetAllTeamSettings :" + docs.length);
+
+            result = docs.find(obj => { return obj.text === teamName; });
+            docs.forEach(
+                function (element) {
+                    temp.push(element);
+                }
+            );
+        });
+
+        if (result === undefined) {
+            console.log("Setting exists.");
+        }
+        else {
+            // Prepare document first
+            var newDoc = {
+                text: teamName
+            };
+
+            dataService.createDocument(TeamSettingsCollectionName, newDoc).then(function (doc) {
+                // Even if no ID was passed to createDocument, one will be generated
+                console.log("SetTeamSetting (CreateTeams) : " + doc.text);
+            });
+
+            console.log("Setting NOT exists.");
+        }
+              
+    });
+}
+
 function GetTeamSettings() {
     // Get data service
     VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
         // Get document by id
-        dataService.getDocument("MyCollection", "MyDocumentId").then(function (doc) {
+        dataService.getDocument(TeamSettingsCollectionName, "MyDocumentId").then(function (doc) {
             // Assuming document has a property named foo
             console.log("Doc foo: " + doc.foo);
         });
     });
 }
 
+var configuredTeams = [];
+
 function GetAllTeamSettings() {
     // Get data service
     VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
         // Get all document under the collection
-        dataService.getDocuments("MyCollection").then(function (docs) {
-            console.log("There are " + docs.length + " in the collection.");
+        dataService.getDocuments(TeamSettingsCollectionName).then(function (docs) {
+            //console.log("There areee " + docs.length + " in the collection in GetAllTeamSettings function.");
+            console.log("GetAllTeamSettings :" + docs.length);
+            docs.forEach(
+                function (element) {
+                    configuredTeams.push(element);
+                }
+            );
         });
-    });
-}
 
-function SetTeamSettings() {
-    // Get data service
-    VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
-        // Prepare document first
-        var newDoc = {
-            fullScreen: false,
-            screenWidth: 500
-        };
-
-        dataService.createDocument("MyCollection", newDoc).then(function (doc) {
-            // Even if no ID was passed to createDocument, one will be generated
-            console.log("Doc id: " + doc.id);
-        });
+        console.log("Set configuredTeams: " + configuredTeams.length);
     });
+
+    return configuredTeams;
 }
 
 function ChangeTeamSettings() {
@@ -135,19 +269,27 @@ function ChangeTeamSettings() {
             screenWidth: 500
         };
 
-        dataService.setDocument("MyCollection", myDoc).then(function (doc) {
+        dataService.setDocument(TeamSettingsCollectionName, myDoc).then(function (doc) {
             console.log("Doc id: " + doc.id);
         });
     });
 }
 
-function DeleteTeamSettings() {
+
+function DeleteTeamSettings(dservice, docId) {
+
+    if (dservice !== null) {
+        dservice.deleteDocument(TeamSettingsCollectionName, docId).then(function () {
+            console.log("Doc verwijderd");
+        });
+    }
+
     // Get data service
     VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
-        var docId = "1234-4567-8910";
+        //var docId = "1234-4567-8910";
         // Delete document
-        dataService.deleteDocument("MyCollection", docId).then(function () {
-            console.log("Doc deleted");
+        dataService.deleteDocument(TeamSettingsCollectionName, docId).then(function () {
+            console.log("Doc verwijderddd");
         });
     });
 }
